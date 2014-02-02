20d12c775296460470e35df650cf64e08b4c55e7
Maaz Ali <faeronsayn@gmail.com>
2014-02-02 01:44:12 -0500

    Get and Insert comment functions have been added
    The Comment class now has the get and insert functions.

--- includes/comment.php
+++ includes/comment.php
@@ -1,30 +1,82 @@
 <?php 
-include 'as-config.php'; 
+include 'cs-config.php'; 
 
-class comment {
+class Comment {
 
-	public $author_id;
-	public $comment_content;
-	public $comment_id;
+	public $id;
+	public $user_id;
+	public $content;
+	public $state;
+	public $likes;
+	public $date;
+
 
  	
-	protected function __constructor($content, $author) {
-		$author_id = $author;
-		$comment_content = $content;
+	protected function __constructor($user_id, $content, $state, $likes, $date, $insert) {
+		$this->user_id = $user_id;
+		$this->content = $content;
+		$this->state = $state;
+		$this->likes = $likes;
+		$this->date = $date;
+
+
+		if ($insert) {
+
+			$this->insert_comment();
+
+		}
+				
+	}
+
+
+	public function insert_comment() {
+
+			$db_con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+				if (mysqli_connect_error()) {
+				    die('Connect Error (' . mysqli_connect_errno() . ') '
+				            . mysqli_connect_error());
+				}
+			
+			$db_con->query('INSERT INTO cs_comments (user_id, comment_date, content, comment_state, likes) VALUES (' . $this->user_id . ', ' . $this->date . ', "' . $this->content . '", "' . $this->state . '", '. $this->likes . ')');
+
+	}
+
+
+
+	public static function get_comment( $comment_id ) {
+
 		$db_con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 			if (mysqli_connect_error()) {
 			    die('Connect Error (' . mysqli_connect_errno() . ') '
 			            . mysqli_connect_error());
 			}
 		
-		$db_con->query('INSERT INTO comments (user_id, comment_date, content, comment_state) VALUES (' . $author_id . ', ' . time() . ', "' . $comment_content . '", "visible")');
-				
-	}
-
-	public function get_comment() {
 
 
+		$comment_info = $db_con->query('SELECT comment_id FROM cs_comments WHERE comment_id = '. $comment_id . ')'->fetch_assoc();
+
+		if ($comment_info) {
+			
+			$id = $comment_id;
+			$user_id = $comment_info['user_id'];
+			$content = $comment_info['content'];
+			$state = $comment_info['comment_state'];
+			$likes = $comment_info['likes'];
+			$date = $comment_info['comment_date'];
+
+			$comment = new Comment($user_id, $content, $state, $likes, $date, false);
+
+			// Initialize the ID of the comment as well
+			$comment->id = $id;
+
+			return $comment;
+
+		} else {
+
+		}
 
 	}
 
 }
+
+?>
